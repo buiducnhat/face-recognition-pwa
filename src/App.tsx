51bc2f5305bb61751ba3edfb1a8b88869f80f1b2
Bui Duc Nhat <nhaths4701@gmail.com>
2023-08-03 17:29:22 +0700

    update: remove react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { Provider as ModalProvider } from '@ebay/nice-modal-react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { observer } from 'mobx-react-lite';
 import { Theme } from 'react-daisyui';
 import { useRoutes } from 'react-router';
@@ -10,21 +8,18 @@ import 'react-toastify/dist/ReactToastify.css';
 import routes from './app/routes';
 import { useStore } from './app/stores';
 
-const queryClient = new QueryClient();
-
 const App = observer(() => {
   const routing = useRoutes(routes);
 
   const { themeStore } = useStore();
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <Theme id="theme-provider" dataTheme={themeStore.themeType}>
         <ModalProvider>{routing}</ModalProvider>
       </Theme>
       <ToastContainer theme={themeStore.isDarkTheme ? 'dark' : 'light'} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    </>
   );
 });
 
